1ae56ba2912082a94a065ef6880640ee9c787424
Kanji-Ishii <ec.2kng@gmail.com>
2021-09-01 10:32:27 +0900

    Exec:rails webpacker:install

--- db/migrate/20210831075201_create_record.rb
+++ db/migrate/20210831075201_create_record.rb
@@ -1,6 +1,14 @@
 class CreateRecord < ActiveRecord::Migration[6.0]
   def change
     create_table :records do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :taining_name, null: false
+      t.integer :weight, null:false, default: 0
+      t.integer :rep_count, null: false, default: 0
+      t.text :note, default: ""
+      t.integer :RM, null: false, null: true, default: 0
+      
+      t.timestamps
     end
   end
 end
