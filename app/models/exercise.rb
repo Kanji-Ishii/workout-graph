5c8aeefa1f3826dcb0ba33415b0eede573296f79
Kanji-Ishii <ec.2kng@gmail.com>
2021-09-15 13:08:23 +0900

    create: exercises destroy action

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,6 +1,6 @@
 class Exercise < ApplicationRecord
-  belongs_to :user, optional: true, dependent: :destroy
-  has_many :records
+  belongs_to :user, optional: true
+  has_many :records, foreign_key: "exercise_id", dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
