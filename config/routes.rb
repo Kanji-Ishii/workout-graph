07413af7346bb538cbee79cadee0e3a0530f9a8f
Kanji-Ishii <ec.2kng@gmail.com>
2021-09-19 22:01:42 +0900

    fix: routing logout from header

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   get '/', to: 'home#index'
-  devise_for :users
   resources :records
   resources :exercises
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords'
+   }
+   #devise_scope :user do
+   #  get 'my_page' => 'users/registrations#my_page'
+   #end
 end
