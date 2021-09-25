6fbf8805a85ae1713039caaad2b663616c01fc33
Kanji-Ishii <ec.2kng@gmail.com>
2021-09-25 11:46:23 +0900

    change: ElasticIP & setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '52.194.165.105', user: 'kanji', roles: %w{app db web} 
+server '35.74.50.190', user: 'kanji', roles: %w{app db web} 
 
 #デプロイするサーバーにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/my-instance-ssh-key-rsa' 
+set :ssh_options, keys: '~/.ssh/gw-rsa' 
