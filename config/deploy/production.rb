25b52c6e54fcad419ce474f54353bebd514f7bfe
Kanji-Ishii <ec.2kng@gmail.com>
2021-09-27 18:21:47 +0900

    fix: change aws instance & resetting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '35.74.50.190', user: 'kanji', roles: %w{app db web} 
+server '54.95.160.35', user: 'kanji', roles: %w{app db web} 
 
 #デプロイするサーバーにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/gw-rsa' 
+set :ssh_options, keys: '~/.ssh/wg-rsa' 
